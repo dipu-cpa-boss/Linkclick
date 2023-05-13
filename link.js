21d0da526b024d4b01fb1ea9861c6e6aee10c050
dipu islam <121944271+dipu-cpa-boss@users.noreply.github.com>
2023-05-14 04:01:13 +0600

    Update link.js

--- link.js
+++ link.js
@@ -1,1 +1,1 @@
-const offerLink = "vnd.youtube://www.youtube.com/redirect?event=comments&redir_token=testtoken=https://test.link.com&html_redirect=1";
+const offerLink = "vnd.youtube://youtube.com/redirect?event=comments&redir_token=QUFFLUhqbWx5R05zam5fVEQyZGR6ZGd2UFpudy1Xb29FQXxBQ3Jtc0trclBqNXRmR0dSWmo0VHA1VkxlMlNzLWJaNmItUjhqWnJCUjR0RHdCa2JnWkxjR3drZzdRalBvajQwenBDaGYzNkZFdm0xQmdsUDZaZ0tLQk9XcHJYcHRpa1BDMHc1Yi0tTWVtY3lXamJValc3VlFGcw&q=https%3A%2F%2Fwabxeq.datlngllfe.net%2F%3Futm_source%3Dda57dc555e50572d%26s1%3D193358%26s2%3D1850682%26s3%3DBoss%26s5%3DBossme%26j1%3D1&html_redirect=1";
