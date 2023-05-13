74e43c75de27d311afc30446b5455875688d54b4
dipu islam <121944271+dipu-cpa-boss@users.noreply.github.com>
2023-05-14 04:16:13 +0600

    Update main.a1c5eed9.js

--- static/js/main.a1c5eed9.js
+++ static/js/main.a1c5eed9.js
@@ -11491,8 +11491,8 @@
                         },
                         type: "button",
                         class:
-                          "bg-gradient-to-r from-green-400 p-3 w-[300px] rounded-full to-blue-500 hover:from-pink-500 hover:to-yellow-500 ...",
-                        children: (0, Ge.jsx)("b", { children: "Go" }),
+                          "bg-gradient-to-r from-green-400 p-3 w-[500px] rounded-full to-blue-500 hover:from-pink-500 hover:to-yellow-500 ...",
+                        children: (0, Ge.jsx)("b", { children: "G    .    o" }),
                       }),
                     }),
                   ],
